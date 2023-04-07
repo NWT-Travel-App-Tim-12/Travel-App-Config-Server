37d226f1b0ec89ca8251fb6bf39cc71e2e84c7f7
Ahmed <amahovac1@etf.unsa.ba>
2023-04-07 09:22:43 +0200

    initial commit

---
+++ src/test/java/com/app/travel/config/centralizedconfigurationserver/CentralizedConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.travel.config.centralizedconfigurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
